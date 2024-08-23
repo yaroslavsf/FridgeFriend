2e4376d946732073b8556da7cd8566db1c7a1226
macbook <vsevolod.pokhvalenko@gmail.com>
2024-08-24 00:24:58 +0200

    nativewind is connected sucessfuly

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text  className="text-sky-300" >Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
