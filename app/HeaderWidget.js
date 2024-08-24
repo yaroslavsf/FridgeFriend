55250c5ec88eafc74d202ea400c220865dc30bd1
BenEliasMartin <martinbenelias@gmail.com>
2024-08-24 11:03:48 +0200

    Fixed Profile Link & Back functionality

--- app/HeaderWidget.js
+++ app/HeaderWidget.js
@@ -8,7 +8,7 @@ const HeaderWidget = ({ title }) => {
     return (
         <View className="flex-row items-center justify-between w-full mt-10">
             <Text className="text-3xl font-bold ml-5">{title}</Text>
-            <Link href="../profile">
+            <Link href="/Profile">
                 <Icon name="person-circle-outline" size={35} color="#000" className="mr-5" />
             </Link>
         </View>
--- app/Profile.js
+++ app/Profile.js
@@ -1,9 +1,19 @@
+import { Link } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
 
 const Profile = () => {
+    const router = useRouter();
     return (
         <View>
+            <TouchableOpacity onPress={() => router.back()} className="mt-4 flex-row items-center">
+                <Icon name="arrow-back" size={24} color="black" />
+                <Text className="ml-2">Back</Text>
+            </TouchableOpacity>
             <Text>Profile Component</Text>
         </View>
     );
